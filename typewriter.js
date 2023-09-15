e2fbe9f1e20b2540faad5bbadd14688f8774615a
Evan Hands <handsonevanoff@gmail.com>
2023-09-14 20:58:10 -0700

    typerwriter() made exportable

--- typewriter.js
+++ typewriter.js
@@ -1,7 +1,11 @@
-const sentence = "animation logic network";
 
-let time = 0;
+/**
+ * typewriter Uses stdout to write a string to console with 50 ms setTimeout per char.
+ * @param {array} sentence A string or array to write to console.
+ * @returns {string} Each char or element appears 50 ms apart in console.
+ */
 const typewriter = function(sentence) {
+  let time = 0;
   sentence += '\n';
   for (const char of sentence) {
     setTimeout(() => {
@@ -10,4 +14,5 @@ const typewriter = function(sentence) {
     time += 50;
   }
 };
-typewriter(sentence);
+
+module.exports = typewriter;
